8d70b5e2d9a7978fd644d3b64cd11f8ef9cc4603
Daniel Goloubev <danielgoloubev@gmail.com>
2017-05-29 14:55:20 +0200

    public_html

---
+++ app.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env nodejs
+var http = require('http');
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Under uppbyggnad!\n');
+}).listen(8080);
+console.log('Server running at http://127.0.0.1:8080/');
