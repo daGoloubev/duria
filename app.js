376f4b4bf7998870d7e5810b13abc2e84051f5f6
Daniel Goloubev <danielgoloubev@gmail.com>
2017-06-05 23:30:25 +0200

    Express static

--- app.js
+++ app.js
@@ -15,12 +15,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images/favicon/', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/images/')));
+app.use(express.static(path.join(__dirname, 'public/javascripts/')));
+app.use(express.static(path.join(__dirname, 'public/stylesheets/')));
 
 app.use('/', index);
 app.use('/users', users);
